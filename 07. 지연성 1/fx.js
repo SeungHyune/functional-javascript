69109abcbaccd829547e20ab646d2b7152824d26
seunghyun <tmdgus7820@naver.com>
2023-05-11 21:12:25 +0900

    range, L.range functional javascript

---
+++ 07. 지연성 1/fx.js
@@ -0,0 +1,39 @@
+// curry
+const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+// map
+const map = curry((f, iter) => {
+    let res = [];
+    for(const a of iter){
+        res.push(f(a))
+    }
+    return res
+});
+
+
+// filter
+const filter = curry((f, iter) => {
+    let res = [];
+    for(const a of iter){
+        if(f(a)) res.push(a)
+    }
+    return res
+});
+
+// reduce
+const reduce = curry((f, acc, iter) => {
+    if(!iter){
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+    }
+    for(const a of iter){
+        acc = f(acc, a);
+    }
+    return acc
+});
+
+// go
+const go = (...args) => reduce((a, f) => f(a), args);
+
+// pipe
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
