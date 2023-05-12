6ead747b337a056371c76586e73babb59522e8e1
seunghyun <tmdgus7820@naver.com>
2023-05-13 03:51:36 +0900

    range, map, filter, take, reduce

--- 07. 지연성 1/fx.js
+++ 07. 지연성 1/fx.js
@@ -2,35 +2,35 @@
 const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
 // map
-const map = curry((f, iter) => {
-    let res = [];
-    for(const a of iter){
-        res.push(f(a))
-    }
-    return res
-});
+// const map = curry((f, iter) => {
+//     let res = [];
+//     for(const a of iter){
+//         res.push(f(a))
+//     }
+//     return res
+// });
 
 
 // filter
-const filter = curry((f, iter) => {
-    let res = [];
-    for(const a of iter){
-        if(f(a)) res.push(a)
-    }
-    return res
-});
+// const filter = curry((f, iter) => {
+//     let res = [];
+//     for(const a of iter){
+//         if(f(a)) res.push(a)
+//     }
+//     return res
+// });
 
 // reduce
-const reduce = curry((f, acc, iter) => {
-    if(!iter){
-        iter = acc[Symbol.iterator]();
-        acc = iter.next().value;
-    }
-    for(const a of iter){
-        acc = f(acc, a);
-    }
-    return acc
-});
+// const reduce = curry((f, acc, iter) => {
+//     if(!iter){
+//         iter = acc[Symbol.iterator]();
+//         acc = iter.next().value;
+//     }
+//     for(const a of iter){
+//         acc = f(acc, a);
+//     }
+//     return acc
+// });
 
 // go
 const go = (...args) => reduce((a, f) => f(a), args);
